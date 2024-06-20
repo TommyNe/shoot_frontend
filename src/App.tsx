bdfd5ed036e20a659e9d82914218b4a7f2e6e701
Thomas Nehrenberg <thomas.nehrenberg@me.com>
2024-06-20 23:38:24 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './style.css';
+
+function App() {
+  const start: boolean = true;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Schützenfest Tinnen 2024
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
